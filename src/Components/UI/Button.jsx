66545f548cfd06793faf3330869577d755799cdb
alekoyunja <alikoyuncu13@gmail.com>
2023-05-01 21:53:39 +0300

    Maaş otm uyarılar eklendi

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
 
 const Button = (props) => {
   return (
-    <button  className={`p-2 text-lg bg-teal-700 text-white mt-3 ${props.className}`}>{props.children}</button>
+    <button 
+    className={`p-2 text-lg bg-teal-700 text-white mt-3 ${props.className}`}
+    type={props.type||"button"}
+    onClick={props.onClick}
+    >
+        {props.children}
+        </button>
   )
 }
 
-export default Button
+export default Button;
