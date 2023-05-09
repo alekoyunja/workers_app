e8068e55e0c6a0c6e31e780646d3a15747e946b9
alekoyunja <alikoyuncu13@gmail.com>
2023-05-09 17:46:44 +0300

    useRef usage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddWorker from "./Components/Workers/AddWorker";
 import WorkerList from "./Components/Workers/WorkerList";
 
 function App() {
   const [workers,setWorkers]=useState([])
   return (
-    <div className="App">
+    <React.Fragment>
       <h1 className="text-white text-center ms-6 text-3xl">Maaş Otomasyonu</h1>
     <AddWorker setWorkers={setWorkers}/>
     <WorkerList workers={workers} setWorkers={setWorkers}/>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/Components/Workers/AddWorker.jsx
+++ src/Components/Workers/AddWorker.jsx
@@ -1,89 +1,90 @@
-import { useState } from "react"
-import Button from "../UI/Button"
-import Card from "../UI/Card"
+import React, { useRef, useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddWorker = (props) => {
+  const [error, setError] = useState();
+  const nameInfoRef = useRef();
+  const wageInputRef=useRef();
 
-const [enteredWorkerName,setEnteredWorkerName]=useState("");
-const [enteredWage,setEnteredWage]=useState("");
-const[error,setError]=useState();
+  const minimumWage = 5000;
 
-  const minimumWage=5000;
+  const addWorkerHandler = (e) => {
+    e.preventDefault();
+    const enteredName=nameInfoRef.current.value;
+    const enteredWage=wageInputRef.current.value;
+    if (nameInfoRef.current.value.trim().length === 0) {
+      setError({
+        title: "İsim Alanı Zorunludur !",
+        message: "Lütfen Bir İsim Giriniz !",
+      });
+      return;
+    }
 
-const addWorkerHandler=(e)=>{
-e.preventDefault();
-
-if(enteredWorkerName.trim().length===0){
-  setError({
-    title:"İsim Alanı Zorunludur !",
-    message:"Lütfen Bir İsim Giriniz !",
-
-  });
-  return;
-}
-
-if(+enteredWage<minimumWage){
-  setError({
-    title:"Maaş Alanı Zorunludur !",
-    message:`Lütfen ${minimumWage} değerinden büyük bir maaş değeri giriniz !`
-
-  });
-  return;
-}  
-props.setWorkers((prevState)=>[
-  {
-    id: Math.floor(Math.random() * 1000), 
-    name:enteredWorkerName,
-    wage:enteredWage,
-  },
-  ...prevState,
-]);
-setEnteredWorkerName("");
-setEnteredWage("");
-};
-const errorHandler = () => {
-setError(null);
-};
-
+    if (+wageInputRef.current.value < minimumWage) {
+      setError({
+        title: "Maaş Alanı Zorunludur !",
+        message: `Lütfen ${minimumWage} değerinden büyük bir maaş değeri giriniz !`,
+      });
+      return;
+    }
+    props.setWorkers((prevState) => [
+      {
+        id: Math.floor(Math.random() * 1000),
+        name: enteredName,
+        wage: enteredWage,
+      },
+      ...prevState,
+    ]);
+    nameInfoRef.current.value="";
+    wageInputRef.current.value="";
+   
+  };
+  const errorHandler = () => {
+    setError(null);
+  };
 
   return (
-    <div>
-      {error&& <ErrorModal onConfirm={errorHandler} error={error}/>}
+    <Wrapper>
+      {error && <ErrorModal onConfirm={errorHandler} error={error} />}
       <Card className="mt-10">
-      <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
-       <label htmlFor="name" className="font-medium ">Çalışan İsmi</label>
+        <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
+          <label htmlFor="name" className="font-medium ">
+            Çalışan İsmi
+          </label>
 
-       <input 
-       type="text" 
-       className="max-w-[40rem] w-full ms-auto border p-2" 
-       placeholder="Çalışan ismi yazınız..." 
-       id="name"
-       onChange={(e)=>setEnteredWorkerName(e.target.value)}
-       value={enteredWorkerName}
-       />
+          <input
+            type="text"
+            className="max-w-[40rem] w-full ms-auto border p-2"
+            placeholder="Çalışan ismi yazınız..."
+            id="name"
+            
+            ref={nameInfoRef}
+            
+          />
 
-       <label 
-       htmlFor="wage" 
-       className="font-medium ">Maaş Miktarı</label>
+          <label htmlFor="wage" className="font-medium ">
+            Maaş Miktarı
+          </label>
 
-       <input 
-       type="number" 
-       className="max-w-[40rem] w-full ms-auto border p-2" 
-       placeholder="Maaş miktarını yazınız..." 
-       id="wage"
-       onChange={(e)=>setEnteredWage(e.target.value)}
-       value={enteredWage}
-       />
-       
-       <Button className="mt-2" type="submit">
-        EKLE
-        </Button>
+          <input
+            type="number"
+            className="max-w-[40rem] w-full ms-auto border p-2"
+            placeholder="Maaş miktarını yazınız..."
+            id="wage"
+            
+            ref={wageInputRef}
+          />
 
-    </form>
-    </Card>
-    </div>
+          <Button className="mt-2" type="submit">
+            EKLE
+          </Button>
+        </form>
+      </Card>
+    </Wrapper>
   );
 };
 
-export default AddWorker
+export default AddWorker;
